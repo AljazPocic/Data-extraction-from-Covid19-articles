8249327781bbe6ca6c7476ddbf48acd921fbceb0
aljazpocic <pocic.aljaz@gmail.com>
2026-08-19 13:13:29 +0200

    Add CovidData schema for structured data extraction

--- prototype.py
+++ prototype.py
@@ -8,6 +8,31 @@ load_dotenv()
 api_key = os.environ["GROQ_API_KEY"]
 
 
+# Schema for extracting data
+from pydantic import BaseModle, Field
+from typing import List, Optional
+
+
+class CovidData(BaseModle):
+    location: str = Field(description="The city, region, or area affected")
+    number_of_deaths: int = Field(
+        description="The number of people dead, beacuse of Covid-19"
+        )
+    num_of_infected: int = Field(
+        description="The number of people infected or sick beacuse of Covid-19"
+        )
+    num_recovered: Optional[int] = Field(
+        default=None, description="The number of people who recoverd from being" \
+        "sick with Covid-19"
+    )
+    list_of_measures: Optional[List[str]] = Field(
+        default=None,
+        description="List of measures taken to slow the spread of the Covid-19 virus"
+    )
+
+
+
+
 with open("articles/01_china_coronavirus_goes_global.txt", "r", encoding="utf-8") as file:
     article_text = file.read()
 
